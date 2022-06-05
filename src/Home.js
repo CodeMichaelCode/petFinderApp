a4030b1ee9704b412ca98ca11e361b3cfae2c807
Michael Olawunmi <molawunmi21@gmail.com>
2022-06-04 23:02:03 -0400

    first commit

--- src/Home.js
+++ src/Home.js
@@ -12,8 +12,3 @@ const Home = () => {
 };
 
 export default Home;
-
-{
-  /* <a href="https://ibb.co/x3x7V01"><img src="https://i.ibb.co/pnYLC8f/dogs-Running.jpg" alt="dogs-Running" border="0"></a>
-<a href="https://ibb.co/qRckjcx"><img src="https://i.ibb.co/CnYh8YW/light-Mode-Logo.png" alt="light-Mode-Logo" border="0"></a> */
-}
